d4b32dc823554c765bde0c417921f7a55e89c11b
sdriver312 <gsteers@gmail.com>
2013-06-05 16:18:47 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleJacqui::Application.config.secret_token = '852a35b4383a33c85e23c7010aa8b5475f7e8b5476fa2371549fb1db30ea5c4800549ae8432e388d9898ab57583f068be5a8a65aa0fd18d84e0b23ad1d25599b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleJacqui::Application.config.session_store :cookie_store, :key => '_sample_jacqui_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SampleJacqui::Application.config.session_store :active_record_store
